d19581674e732c0e895d0cff7c59efe392448bd1
Kerry Buckley <kerryjbuckley@gmail.com>
2010-10-01 10:47:46 +0100

    Block non-diagonals, then play preferred space

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,6 +1,40 @@
+def play x, y
+  puts y * 3 + x
+  exit
+end
+
+def must_block_row x, y
+  board[y].count(them) == 2
+end
+
+def must_block_column x, y
+  [board[0][x], board[1][x], board[2][x]].count(them) == 2
+end
+
+def must_block_diagonal x, y
+  false
+end
+
 input = ARGV[0]
 
 chars = input.split(//)
 board = [chars[0..2], chars[3..5], chars[6..8]]
 
-puts 4
+if chars.count("0") > chars.count("x")
+  me = "x"
+  them = "0"
+else
+  me = "0"
+  them = "x"
+end
+
+board.each_with_index do |row, y|
+  row.each_with_index do |cell, x|
+    next unless cell == "-"
+    play x, y if must_block_row(x, y) || must_block_column(x, y) || must_block_diagonal(x, y)
+  end
+end
+
+[4, 0, 2, 6, 8, 1, 3, 5, 7].each do |n|
+  puts n if input[n] == "-"
+end
